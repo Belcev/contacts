325ca23a79e7763f3ed49099c5d7b3dd489a00e8
Roman Belčev <roman.belcev@unnits.com>
2025-10-19 14:20:08 +0200

    tests

--- ecs.php
+++ ecs.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use SlevomatCodingStandard\Sniffs\Functions\StaticClosureSniff;
+use SlevomatCodingStandard\Sniffs\Namespaces\ReferenceUsedNamesOnlySniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
@@ -23,6 +25,16 @@ return ECSConfig::configure()
     ->withCache(
         directory: __DIR__ . '/tmp/ecs',
     )
+    ->withSkip([
+        __DIR__ . '/bootstrap/cache',
+        ReferenceUsedNamesOnlySniff::class => [
+            __DIR__ . '/ecs.php',
+            __DIR__ . '/rector.php',
+        ],
+        StaticClosureSniff::class => [
+            __DIR__ . '/database/factories',
+        ],
+    ])
     ->withPhpCsFixerSets(psr12: true)
     ->withSpacing(indentation: Option::INDENTATION_SPACES, lineEnding: PHP_EOL)
 
